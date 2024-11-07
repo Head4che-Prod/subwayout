ffd2ca123a3d770197e1968d84fe91a828b45569
Cr0usti-bat <Tidianebath@gmail.com>
2024-11-07 17:22:48 +0100

    Character is Moving Properly
    Speed and drag value can still be modified

--- SubWay Out/Assets/Scripts/PlayerCam.cs
+++ SubWay Out/Assets/Scripts/PlayerCam.cs
@@ -32,5 +32,8 @@ public class PlayerCam : MonoBehaviour
 		//rotate cam and orientation
 		transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
 		orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+		
     }
+    
+    
 }
