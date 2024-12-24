36d0733142bd747d71ff14f1501b7d9f76cd6c8f
Adam-Graziani <adam.graziani@epita.fr>
2024-12-24 11:14:33 +0100

    Trying to make buttons functionnal on disconnect

--- SubWay Out/Assets/Prefabs/UI/NetworkDebugUI/NetworkDebugUI.cs
+++ SubWay Out/Assets/Prefabs/UI/NetworkDebugUI/NetworkDebugUI.cs
@@ -1,12 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
-using System.Data;
 
-public class NetworkDebugUI : MonoBehaviour
+public class NetworkDebugUI : NetworkBehaviour
 {
     [SerializeField] Button ConnectClientButton;
     [SerializeField] Button ConnectHostButton;
+
+    private NetworkVariable<bool> buttonsActive = new NetworkVariable<bool>(true, NetworkVariableReadPermission.Owner, NetworkVariableWritePermission.Owner);
+
+
+    public override void OnNetworkSpawn()
+    {
+        buttonsActive.OnValueChanged += (bool _, bool _) => ManageButtons();
+        NetworkManager.Singleton.OnConnectionEvent += (NetworkManager _, ConnectionEventData eventData) => buttonsActive.Value = eventData.ClientId == NetworkObjectId;
+    }
+
 
     private void Start()
     {
@@ -16,33 +26,28 @@ public class NetworkDebugUI : MonoBehaviour
         Button connectClientButton = ConnectClientButton.GetComponent<Button>();
         connectClientButton.onClick.AddListener(ConnectClient);
 
-        NetworkManager.Singleton.OnClientDisconnectCallback += ResetButtons;   // Resets button on disconnect
+        buttonsActive.Value = true;
     }
 
     public void ConnectHost()
     {
         NetworkManager.Singleton.StartHost();
-        DisactivateButtons();
+        buttonsActive.Value = false;
+        ManageButtons();
         Debug.Log("Host connected successfully");
     }
 
     public void ConnectClient()
     {
         NetworkManager.Singleton.StartClient();
-        DisactivateButtons();
+        buttonsActive.Value = false;
+        ManageButtons();
         Debug.Log("Client connected successfully");
     }
 
-    private void DisactivateButtons()
+    private void ManageButtons()
     {
-        ConnectHostButton.GetComponent<Button>().interactable = false;
-        ConnectClientButton.GetComponent<Button>().interactable = false;
-    }
-
-    // Will need to be changed once the UI is player-specific
-    private void ResetButtons(ulong ClientID)
-    {
-        ConnectHostButton.GetComponent<Button>().interactable = true;
-        ConnectClientButton.GetComponent<Button>().interactable = true;
+        ConnectHostButton.GetComponent<Button>().interactable = buttonsActive.Value;
+        ConnectClientButton.GetComponent<Button>().interactable = buttonsActive.Value;
     }
 }
