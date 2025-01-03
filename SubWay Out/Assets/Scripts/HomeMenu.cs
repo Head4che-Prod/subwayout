9d8c8f5677ad88b8d571aa275a7238a37862e894
Arthur Fiolet <fioletarthur@gmail.com>
2025-01-03 18:08:51 +0100

    Terrible-looking and not functionnal home screen

---
+++ SubWay Out/Assets/Scripts/HomeMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class HomeMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateGame()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+}
