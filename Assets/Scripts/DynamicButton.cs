c96432fca42874e7ff0850feae697a9923455c10
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-01-25 07:18:45 +0100

    Demo menu (#33)
    * UI Done
    
    Left to implement :
    * Buttons functionalities (start scenes)
    * Settings screen
    * Try to replace the background by the blurried metro img
    
    * Name changes
    
    Late ctrl s ;-;
    
    * Button functionalites
    
    * Début menu de pause
    
    ___ (si valentin tu vois ça me tue pas)
    
    * Pause menu done !

---
+++ Assets/Scripts/DynamicButton.cs
@@ -0,0 +1,33 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DynamicButton : MonoBehaviour, IPointerEnterHandler, IDeselectHandler, ISelectHandler
+{
+    private Selectable selectable;
+	private GameObject childToAppear; // The child GameObject to toggle
+    
+	private void Awake()
+    {
+        // Automatically retrieve the Selectable component (e.g., Button, Toggle) attached to the same GameObject
+        selectable = GetComponent<Selectable>();
+		childToAppear = transform.Find("AppearOnSelect").gameObject;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		selectable.Select();
+	}
+
+	public void OnDeselect(BaseEventData eventData)
+    {
+		childToAppear.SetActive(false);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+		childToAppear.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuOpener : MonoBehaviour
+{
+    private GameObject pauseMenuUI;
+    [SerializeField]
+    public GameObject gameElements;
+
+    private bool isPaused = false;
+
+    void Awake() 
+    {
+        pauseMenuUI = transform.Find("PauseMenuUI").gameObject;
+        pauseMenuUI.SetActive(false);
+        gameElements.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        gameElements.SetActive(true);
+        Time.timeScale = 1f; // Resume game time
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        gameElements.SetActive(false);
+        Time.timeScale = 0f; // Freeze game time
+        isPaused = true;
+        foreach (Selectable button in Selectable.allSelectablesArray)
+            if (button.name == "ResumeButton")
+                button.Select();
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Scenes/DemoMenu");
+    }
+}
