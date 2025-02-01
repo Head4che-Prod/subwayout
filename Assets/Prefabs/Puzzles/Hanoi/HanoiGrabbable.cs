5315eb403ec43be49de0476864af177cc8c1405d
Adam-Graziani <adam.graziani@epita.fr>
2025-02-01 12:37:27 +0100

    Fixed Hanoi plane management
    Hanoi Towers can now lock object movement to plane in order to enable smoother movement.
    Models still need to be updated.

--- Assets/Prefabs/Puzzles/Hanoi/HanoiGrabbable.cs
+++ Assets/Prefabs/Puzzles/Hanoi/HanoiGrabbable.cs
@@ -17,17 +17,17 @@ namespace Prefabs.Puzzles.Hanoi
                     throw new NullReferenceException("Could not find camera holding the hanoi element.");
                 
                 // Computes the vector normal to the game puzzle plane.
-                Vector3 normal = (HanoiTowers.ActiveTowersGameObject.transform.rotation * Quaternion.Euler(90, 0, 0)) * Vector3.up;  
+                Vector3 normal = HanoiTowers.ActiveTowersGameObject.transform.rotation * Vector3.forward;  
                 float a = normal.x;
                 float b = normal.y;
                 float c = normal.z;
                 Debug.Log($"{a}x + {b}y + {c}z");
 
                 
-                // Point in the plane
-                float xT = HanoiTowers.ActiveTowersGameObject.transform.position.x;
-                float yT = HanoiTowers.ActiveTowersGameObject.transform.position.y;
-                float zT = HanoiTowers.ActiveTowersGameObject.transform.position.z;
+                // A point in the plane (corresponding to the object you are currently holding)
+                float xT = Rb.position.x;
+                float yT = Rb.position.y;
+                float zT = Rb.position.z;
                 
                 // Points forming the raycast
                 float xA = HolderCameraTransform.position.x;
@@ -40,18 +40,11 @@ namespace Prefabs.Puzzles.Hanoi
                 
                 float d = -(a * xT + b * yT + c * zT);
                 
-                // Don't ask me how the debug works. Trust me, it does NOT NO IT DOESNT THATS WHY IT DIDNT WORK.
-                DebugPlane.Instance.transform.rotation = Quaternion.AngleAxis(
-                    Mathf.Acos(Vector3.Dot(Vector3.up, normal.normalized)) * Mathf.Rad2Deg,
-                    Vector3.Cross(Vector3.up, normal.normalized)
-                );
-                DebugPlane.Instance.transform.position = new Vector3(0, 0, -d/c);
+                DebugPlane.Instance.transform.rotation = Quaternion.LookRotation(normal, Vector3.up) * Quaternion.Euler(90, 0, 0); // A plane's default face is the y face, but we want to see the z face for debugging purposes
+                DebugPlane.Instance.transform.position = new Vector3(xT, yT, zT);
                 GrabPointVisualizer.Instance.transform.position = GrabPointTransform.position;
                 MoveLine.Instance.SetPosition(0, new Vector3(xA, yA, zA));
                 MoveLine.Instance.SetPosition(1, new Vector3(xB, yB, zB));
-                
-                // Rotating 90 degrees along the x axis to account for Hanoi's base shape
-                //(yA, yB, zA, zB) = (-zA, -zB, yA, yB);
                 
                 // Compute intersection between ray and plane
                 
@@ -60,16 +53,17 @@ namespace Prefabs.Puzzles.Hanoi
                 if (denominator == 0f)
                     return new Vector3(xT, yT, zT);
                 
-                float numerator = a * xA + b * yA + c * zA + d;
+                float numerator = -(a * xA + b * yA + c * zA + d);
                 float t = numerator / denominator;
 
-                Vector3 resultVectorRotated = new Vector3(
+                Vector3 intersection = new Vector3(
                     xA + t * (xB - xA),
                     yA + t * (yB - yA),
                     zA + t * (zB - zA)
                 );
 
-                return resultVectorRotated;
+                MoveLine.Instance.SetPosition(2, intersection);
+                return intersection;
             }
         }
         
