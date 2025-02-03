ce3130167132b45550c25f379e1b977653832678
Valentin_Kh <valentin_kh@outlook.fr>
2025-02-03 21:31:13 +0100

    Revert "Squashed commit of the following:"
    This reverts commit 01aa37410862418c8ab5b00df9748e4edf64ead6.

--- Assets/Prefabs/Puzzles/Hanoi/HanoiException.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-public class HanoiException : Exception
-{
-    public HanoiException(string s) : base(s) { }
-}
---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,8 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PickupScript : NetworkBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+}
