db2b70976d0e45655043fc7a8c187c333dec2c07
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-02-22 18:51:02 +0100

    Unity new input system
    Switch to NewInputSystem for the debug console

--- Assets/Prefabs/UI/DebugConsole/DebugConsole.cs
+++ Assets/Prefabs/UI/DebugConsole/DebugConsole.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
 public class DebugConsole : MonoBehaviour
 {
@@ -13,8 +12,6 @@ public class DebugConsole : MonoBehaviour
     [SerializeField] Selectable InputField;
     private static Dictionary<string, Action> commands;
     private static bool isActivated = false;
-    private static int touchCount = 0;
-    private static float lastTouch = 0;
 
     void Awake()
     {
@@ -26,38 +23,21 @@ public class DebugConsole : MonoBehaviour
 
             commands = new Dictionary<string, Action>();
             commands.Add("sayHello", () => Log("Hello, world !"));
-
-            touchCount = 0;
-            lastTouch = 0;
         }
 
         gameObject.transform.GetChild(0).gameObject.SetActive(isActivated);
     }
 
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.T))
-        {
+    public void focusOnConsole(InputAction.CallbackContext context) {
+        if (context.performed)
             InputField.Select();
-        }
+    }
 
-        if (Input.GetKeyUp(KeyCode.RightShift))
-        {
-            if (Time.time - lastTouch < .5)
-            {
-                touchCount++;
-            }
-            else
-            {
-                touchCount = 0;
-            }
-            lastTouch = Time.time;
-            if (touchCount >= 5)
-            {
-                isActivated = !isActivated;
-                touchCount = 0;
-                gameObject.transform.GetChild(0).gameObject.SetActive(isActivated);
-            }
+    public void onModeActivationCommand(InputAction.CallbackContext context)
+    {
+        if (context.performed) {
+            isActivated = !isActivated;
+            gameObject.transform.GetChild(0).gameObject.SetActive(isActivated);
         }
     }
 
