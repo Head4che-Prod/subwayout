c3edda5e94ddf9764ffd3663fd3d6af3ba5e3dea
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-02-24 15:33:19 +0100

    Use newInputSystem
    Also fixed a few bugs

--- Assets/Prefabs/Player/PlayerUI/DebugConsole/DebugConsole.cs
+++ Assets/Prefabs/Player/PlayerUI/DebugConsole/DebugConsole.cs
@@ -17,8 +17,9 @@ namespace Prefabs.Player.PlayerUI.DebugConsole
         private List<string> _commandHistory;
         private int _commandHistoryIndex;
         private string _tempCommand;
-        
-        private static bool _isActivated;
+        private bool _ignoreNextInput = false;
+
+        public static bool IsActivated { get; private set; }
         private string _previousInputMap;
         private string _currentText;
 
@@ -39,19 +40,19 @@ namespace Prefabs.Player.PlayerUI.DebugConsole
             {
                 Singleton = this;
 
-                Commands.Add("sayHello", () => Log("Hello, world!"));
-                Commands.Add("inputMode", () => Log(_player.Input.currentActionMap.name));
-                Commands.Add("help", () => Log("Available commands:\n - " + String.Join("\n - ", Commands.Keys)));
+                Commands["sayHello"] = () => Log("Hello, world!");
+                Commands["inputMode"] = () => Log(_player.Input.currentActionMap.name);
+                Commands["help"] = () => Log("Available commands:\n - " + String.Join("\n - ", Commands.Keys));
             }
 
-            gameObject.transform.GetChild(0).gameObject.SetActive(_isActivated);
+            gameObject.transform.GetChild(0).gameObject.SetActive(IsActivated);
         }
 
         public void Start()
         {
             _player = GetComponentInParent<PlayerObject>();
-            _isActivated = false;
-            
+            IsActivated = false;
+
             _showConsoleAction = _player.Input.actions["ShowConsole"];
             _focusConsoleAction = _player.Input.actions["ConsoleFocus"];
             _cancelAction = _player.Input.actions["DebugConsoleCancel"];    // Names are distinct as to not interfere with other maps
@@ -66,36 +67,37 @@ namespace Prefabs.Player.PlayerUI.DebugConsole
             _backspaceAction.performed += _ => Backspace();
             _upAction.performed += _ => NavigateUpHistory();
             _downAction.performed += _ => NavigateDownHistory();
-            
-            
+
+
             _previousInputMap = _player.Input.currentActionMap.name;
             _commandHistory = new List<string>();
             _commandHistoryIndex = -1;
             _tempCommand = "";
-            
+
             _currentText = "";
         }
 
         private void ToggleConsole(InputAction.CallbackContext ctx)
         {
-            _isActivated = !_isActivated;   
-            gameObject.transform.GetChild(0).gameObject.SetActive(_isActivated);
+            IsActivated = !IsActivated;
+            gameObject.transform.GetChild(0).gameObject.SetActive(IsActivated);
         }
 
         private void FocusOnConsole(InputAction.CallbackContext context)
         {
-            if (_isActivated)
+            if (IsActivated)
             {
-                inputField.Select();
                 _previousInputMap = _player.Input.currentActionMap.name;
                 _player.InputManager.SetPlayerInputMap("DebugConsole");
                 Keyboard.current.onTextInput += HandleCommandInput;
                 _commandHistoryIndex = -1;
                 _tempCommand = "";
+                inputField.Select();
+                _ignoreNextInput = true;
             }
         }
 
-        private void FocusOffConsole() 
+        private void FocusOffConsole()
         {
             _player.InputManager.SetPlayerInputMap(_previousInputMap);
             inputField.text = "";
@@ -106,7 +108,7 @@ namespace Prefabs.Player.PlayerUI.DebugConsole
 
         private void HandleCommandInput(char c)
         {
-            if (Char.IsLetter(c) || c == ' ')
+            if ((Char.IsLetter(c) || c == ' ') && !_ignoreNextInput)
             {
                 if (!(Keyboard.current.shiftKey.isPressed ^
                       Keyboard.current.capsLockKey.isPressed)) // If caps lock XNOR shift
@@ -114,6 +116,8 @@ namespace Prefabs.Player.PlayerUI.DebugConsole
                 _currentText += c;
                 inputField.text = _currentText;
             }
+
+            _ignoreNextInput = false;
         }
 
         private void Backspace()
@@ -156,13 +160,16 @@ namespace Prefabs.Player.PlayerUI.DebugConsole
                 _commandHistoryIndex = -1;
             }
         }
-        
+
         public void ExecCommand()
         {
-            if (_currentText.Trim() != "" && (_commandHistory.Count == 0 || _currentText != _commandHistory[0]))
-                _commandHistory.Insert(0, _currentText);
-            Commands.GetValueOrDefault(_currentText, () => LogError("Command doesn't exist"))();
-            FocusOffConsole();
+            if (inputField.isFocused)
+            {
+                if (_currentText.Trim() != "" && (_commandHistory.Count == 0 || _currentText != _commandHistory[0]))
+                    _commandHistory.Insert(0, _currentText);
+                Commands.GetValueOrDefault(_currentText, () => LogError("Command doesn't exist"))();
+                FocusOffConsole();
+            }
         }
 
         public static void AddCommand(string name, Action func)
--- Assets/Prefabs/UI/PauseMenu/PauseMenu.cs
+++ Assets/Prefabs/UI/PauseMenu/PauseMenu.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using Prefabs.Player;
+using Prefabs.Player.PlayerUI.DebugConsole;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -6,10 +11,13 @@ namespace Prefabs.UI.PauseMenu
 {
     public class PauseMenu : MonoBehaviour
     {
+        private InputAction _pauseAction;
+
         private GameObject _pauseMenuUI;
         private GameObject _gameElements;
 
         private bool _isPaused = false;
+        private List<Selectable> _toReactivate;
 
         private void SetGameElementsAttribute()
         {
@@ -29,17 +37,15 @@ namespace Prefabs.UI.PauseMenu
             SetGameElementsAttribute();
             _pauseMenuUI.SetActive(false);
             _gameElements.SetActive(true);
-        }
-
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+            _pauseAction = GetComponentInParent<PlayerObject>().Input.actions["Pause"];
+            _pauseAction.performed += _ =>
             {
                 if (_isPaused)
                     Resume();
                 else
                     Pause();
-            }
+            };
+            _toReactivate = new List<Selectable>();
         }
 
         public void Resume()
@@ -48,6 +54,10 @@ namespace Prefabs.UI.PauseMenu
             _gameElements.SetActive(true);
             Time.timeScale = 1f; // Resume game time
             _isPaused = false;
+            foreach (Selectable selectable in _toReactivate)
+            {
+                selectable.interactable = true;
+            }
         }
 
         void Pause()
@@ -56,9 +66,15 @@ namespace Prefabs.UI.PauseMenu
             _gameElements.SetActive(false);
             Time.timeScale = 0f; // Freeze game time
             _isPaused = true;
+            _toReactivate = new List<Selectable>();
             foreach (Selectable button in Selectable.allSelectablesArray)
                 if (button.name == "ResumeButton")
                     button.Select();
+                else if (!button.transform.IsChildOf(_pauseMenuUI.transform))
+                {
+                    _toReactivate.Add(button);
+                    button.interactable = false;
+                }
         }
 
         public void QuitGame()
