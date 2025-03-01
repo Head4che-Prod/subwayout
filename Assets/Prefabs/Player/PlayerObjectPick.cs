b358a92b7f98bb311e25be7eeadbdabb4a680450
Valentin_Kh <valentin_kh@outlook.fr>
2025-03-01 12:33:41 +0100

    Revert "Merge branch 'main' into Multiplayer-Menu"
    This reverts commit d08456538b424b140ee1babcce70946158000e2d, reversing
    changes made to cd3990825e1dc3e02a76527919d3ce2575e9cea5.

---
+++ Assets/Prefabs/Player/PlayerObjectPick.cs
@@ -0,0 +1,47 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Prefabs.Player
+{
+    public class PlayerObjectPick : NetworkBehaviour
+    {
+        [SerializeField] private int reach;
+        
+        private PlayerObject _player;
+        private ObjectGrabbable _objectGrabbable;
+        private InputAction _interactInput;
+
+        private void Start()
+        {
+            _player = GetComponent<PlayerObject>();
+            _interactInput = _player.Input.actions["Interact"];
+        }
+
+        private void Update()
+        {
+            Debug.DrawRay(_player.playerCamera.transform.position, _player.playerCamera.transform.forward * reach, Color.blue);
+            if (_interactInput.WasPressedThisFrame())
+            {
+                if (_objectGrabbable is null) // No item in hand
+                {
+                    if (Physics.Raycast(_player.playerCamera.transform.position,
+                            _player.playerCamera.transform.forward, out RaycastHit raycastHit, reach))
+                    {
+                        // Debug.Log(raycastHit.collider.gameObject.name);
+                        if (raycastHit.transform.TryGetComponent(out ObjectGrabbable objGrabbable) && objGrabbable.Grabbable)
+                        {
+                            _objectGrabbable = objGrabbable;
+                            _objectGrabbable.Grab(_player.grabPointTransform, _player.playerCamera.transform);
+                        }
+                    }
+                }
+                else
+                {
+                    _objectGrabbable.Drop();
+                    _objectGrabbable = null;
+                }
+            }
+        }
+    }
+}
