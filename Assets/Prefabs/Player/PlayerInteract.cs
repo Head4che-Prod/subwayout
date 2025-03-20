04e846c7cce29026868bf0d2a688d7800110805f
Valentin_Kh <valentin_kh@outlook.fr>
2025-03-20 11:27:27 +0100

    Separate interactions behaviors handlers

--- Assets/Prefabs/Player/PlayerInteract.cs
+++ Assets/Prefabs/Player/PlayerInteract.cs
@@ -23,89 +23,65 @@ namespace Prefabs.Player
 
             try
             {
-                _actionInput.performed += HandlePress;
-                _actionInput.canceled += HandleRelease;
-                _grabInput.performed += HandlePress;
-                _grabInput.canceled += HandleRelease;
+                _actionInput.performed += _ => HandleAction();
+                _grabInput.performed += _ => HandleGrab();
             }
             catch (NullReferenceException e)
             {
                 Debug.LogError(e.Message);
             }
         }
-        
-        /// <summary>
-        /// This method handles button press.
-        /// </summary>
-        /// <param name="context"><see cref="InputAction"/>'s <see cref="InputAction.CallbackContext"/> of the press</param>
-        private void HandlePress(InputAction.CallbackContext context)
-        {
-            Debug.DrawRay(player.playerCamera.transform.position, player.playerCamera.transform.forward * reach, Color.blue);
-            /*
-            RaycastHit[] hits = new RaycastHit[2];
-            int hitCount = Physics.RaycastNonAlloc(playerCamera.transform.position, playerCamera.transform.forward, hits, reach);
-            */
 
+        private void HandleAction()
+        {
+            Debug.DrawRay(
+                player.playerCamera.transform.position, 
+                player.playerCamera.transform.forward * reach, 
+                Color.red
+            );
+            
             Physics.Raycast(
                 player.playerCamera.transform.position,
                 player.playerCamera.transform.forward,
-                out RaycastHit hit,
+                out RaycastHit actionHit,
                 reach
             );
             
-            // Handle the first raycast hit
-            if (/*hits[0]*/hit.transform is not null && /*hits[0]*/hit.transform.TryGetComponent(out ObjectInteractable interactable))
+            if (actionHit.transform is not null && actionHit.transform.TryGetComponent(out ObjectActionable actionable))
+                actionable.HandleAction(player);
+        }
+        
+        private void HandleGrab()
+        {
+            Debug.DrawRay(
+                player.playerCamera.transform.position, 
+                player.playerCamera.transform.forward * reach, 
+                Color.blue
+            );
+            
+            Physics.Raycast(
+                player.playerCamera.transform.position,
+                player.playerCamera.transform.forward,
+                out RaycastHit grabHit,
+                reach
+            );
+
+            if (grabHit.transform is not null && grabHit.transform.TryGetComponent(out ObjectGrabbable grabbable))
             {
-                // Action an object
-                if (context.action.id == _actionInput.id && interactable is ObjectActionable objActionable) 
-                {   
-                    objActionable.HandleAction(player);
-                }
+                if (grabbable == GrabbedObject)
+                    GrabbedObject.Drop();
                 
                 // Grab an object
-                else if (context.action.id == _grabInput.id &&
-                         interactable is ObjectGrabbable { Grabbable: true } objGrabbable &&
+                else if (grabbable is { Grabbable: true } &&
                          GrabbedObject is null)
                 {
-                    GrabbedObject = objGrabbable;
+                    GrabbedObject = grabbable;
                     GrabbedObject.Grab(player);
-                    return;
-                }
-            } 
-            
-            if (GrabbedObject is not null && context.action.id == _grabInput.id)
-            {
-                // Place an object
-                
-                /*
-                if (
-                    hitCount > 1 &&
-                    hits[1].transform is not null &&
-                    hits[1].transform.TryGetComponent(out ObjectPlaceholder placeholder) &&
-                    placeholder.Free &&
-                    _grabbedObject.ConvertActionableType == placeholder.actionableType
-                )
-                {
-                    _grabbedObject.ToActionable(placeholder);
-                    _grabbedObject = null;
-                }
-                */
-            
-                // Drop an object
-                
-                // else
-                {
-                    GrabbedObject.Drop();
                 }
             }
-        }
-
-        /// <summary>
-        /// This method handles button release.   // Why tho?
-        /// </summary>
-        /// <param name="context"><see cref="InputAction"/>'s <see cref="InputAction.CallbackContext"/> of the release</param>
-        private void HandleRelease(InputAction.CallbackContext context)
-        {
+            else if (GrabbedObject is not null)
+                GrabbedObject.Drop();
+            
         }
     }
 }
