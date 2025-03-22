e981316da03268b80096ae644345e765a15310a2
Cospiz67 <jeannedurand2707@gmail.com>
2025-03-22 21:13:01 +0100

    some fixes in code

--- Assets/Prefabs/Player/PlayerSelectionAnimation.cs
+++ Assets/Prefabs/Player/PlayerSelectionAnimation.cs
@@ -7,19 +7,16 @@ public class PlayerSelectionAnimation : MonoBehaviour
     private void Awake()
     {
         _startPosition = transform.position; // set to the offscreen pos
-        
     }
 
     private void Update()
     {
         transform.position = Vector3.Lerp(transform.position, finalPosition, 0.03f); //move the object between pos and finalpos
-        
     }
 
     private void OnDisable()
     {
         transform.position = _startPosition; //when disabled pos returns to the start one (offscreen one)
-        
     }
     
     
