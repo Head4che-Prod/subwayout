1d0b280e137416d532922228d84e8b93a8e6098b
Valentin_Kh <valentin_kh@outlook.fr>
2025-03-22 19:11:01 +0100

    Improve raycasts management

--- Assets/Prefabs/Player/PlayerInteract.cs
+++ Assets/Prefabs/Player/PlayerInteract.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Objects;
 using Unity.Netcode;
 using UnityEngine;
@@ -34,54 +35,89 @@ namespace Prefabs.Player
 
         private void HandleAction()
         {
+            Debug.Log("Try to action");
+
+            RaycastHit actionHit;
+            try
+            {
+                RaycastHit[] results = new RaycastHit[3];
+                Physics.RaycastNonAlloc(player.playerCamera.transform.position, player.playerCamera.transform.forward,
+                    results, reach);
+                actionHit = results.First(hit => hit.transform.TryGetComponent<ObjectActionable>(out _));
+            }
+            catch (NullReferenceException)
+            {
+                return;
+            }
+            
             Debug.DrawRay(
                 player.playerCamera.transform.position, 
-                player.playerCamera.transform.forward * reach, 
+                player.playerCamera.transform.forward * actionHit.distance, 
                 Color.red
             );
-            
-            Physics.Raycast(
-                player.playerCamera.transform.position,
-                player.playerCamera.transform.forward,
-                out RaycastHit actionHit,
-                reach
-            );
-            
+
             if (actionHit.transform is not null && actionHit.transform.TryGetComponent(out ObjectActionable actionable))
+            {
+                Debug.Log($"Action {actionable.name}");
                 actionable.HandleAction(player);
+            }
         }
         
         private void HandleGrab()
         {
+            Debug.Log("Try to grab");
+            
+            
+            RaycastHit grabHit;
+            try
+            {
+                RaycastHit[] hits = new RaycastHit[3];
+                Physics.RaycastNonAlloc(
+                    player.playerCamera.transform.position, 
+                    player.playerCamera.transform.forward, 
+                    hits, 
+                    reach
+                );
+                grabHit = hits
+                    .First(hit => hit.transform.TryGetComponent<ObjectGrabbable>(out _));
+            }
+            catch (NullReferenceException)
+            {
+                if (GrabbedObject is not null)
+                {
+                    Debug.Log($"Drop {GrabbedObject.name}");
+                    GrabbedObject.Drop();
+                }
+                return;
+            }
+            
             Debug.DrawRay(
                 player.playerCamera.transform.position, 
-                player.playerCamera.transform.forward * reach, 
+                player.playerCamera.transform.forward * grabHit.distance, 
                 Color.blue
-            );
-            
-            Physics.Raycast(
-                player.playerCamera.transform.position,
-                player.playerCamera.transform.forward,
-                out RaycastHit grabHit,
-                reach
             );
 
             if (grabHit.transform is not null && grabHit.transform.TryGetComponent(out ObjectGrabbable grabbable))
             {
                 if (grabbable == GrabbedObject)
+                {
+                    Debug.Log($"Drop pointed {GrabbedObject.name}");
                     GrabbedObject.Drop();
+                }
                 
                 // Grab an object
-                else if (grabbable is { Grabbable: true } &&
-                         GrabbedObject is null)
+                else if (grabbable is { Grabbable: true } && GrabbedObject is null)
                 {
+                    Debug.Log($"Grab {grabbable.name}");
                     GrabbedObject = grabbable;
                     GrabbedObject.Grab(player);
                 }
             }
             else if (GrabbedObject is not null)
+            {
+                Debug.Log($"Drop {GrabbedObject.name}");
                 GrabbedObject.Drop();
-            
+            }
         }
     }
 }
