e82f3634b442841cf7279a3c2c99a1fd29418f44
Cospiz67 <jeannedurand2707@gmail.com>
2025-03-22 20:53:05 +0100

    buttons updates
    - disable the buttons if we are on the last/ first model

--- Assets/Prefabs/Player/PlayerSelection.cs
+++ Assets/Prefabs/Player/PlayerSelection.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.UI; //useful for the buttons
 public class PlayerSelection : MonoBehaviour
 {
     private int currentPlayer;
+    [SerializeField] private Button previousPlayerButton;
+    [SerializeField] private Button nextPlayerButton;
+
+    private void Awake()
+    {
+        SelectMyPlayer(0); //when we start, we are sure that we have one player activated
+    }
     private void SelectMyPlayer(int _index)
     {
+        previousPlayerButton.interactable = (_index != 0); //set the "previous" button to interactable only if we're not on the first model
+        nextPlayerButton.interactable = (_index != transform.childCount-1); //set the "next" button to interactable only if we're not on the last model
         for (int i = 0; i < transform.childCount; i++) //for loop that iterates until no children
         {
             transform.GetChild(i).gameObject.SetActive(i == _index); // will activate the child of the index
