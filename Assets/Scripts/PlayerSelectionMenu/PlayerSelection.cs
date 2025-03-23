8a30c088e822bc74cbf347f210871d4dd66d8059
Adam-Graziani <adam.graziani@epita.fr>
2025-03-23 19:31:38 +0100

    Optimisaiton fixes
    Reduces GetChild usage to minimum

--- Assets/Scripts/PlayerSelectionMenu/PlayerSelection.cs
+++ Assets/Scripts/PlayerSelectionMenu/PlayerSelection.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Prefabs.Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,34 +9,38 @@ namespace Prefabs.UI
 {
     public class PlayerSelection : MonoBehaviour
     {
-        private static int currentPlayer = 0;
+        private int _currentPlayer = 0;
+        private GameObject[] _playerModels;
         [SerializeField] private Button previousPlayerButton;
         [SerializeField] private Button nextPlayerButton;
-        [SerializeField] public GameObject prefabPlayer;
+        [SerializeField] private PlayerObject prefabPlayer;
 
         private void Awake()
         {
-            for (int i = 0; i < transform.childCount; i++)
+            int childCount = transform.childCount;
+            _playerModels = new GameObject[childCount];
+            for (int i = 0; i < childCount; i++)
             {
-                transform.GetChild(i).gameObject.SetActive(i==currentPlayer);
+                _playerModels[i] = transform.GetChild(i).gameObject;
+                _playerModels[i].SetActive(i == _currentPlayer);
             }
         }
     
         public void ChangeMyPlayer(int change) //for my buttons < and >
         {
-            transform.GetChild(currentPlayer).gameObject.SetActive(false);
-            currentPlayer += change + transform.childCount; //will set the index to + change 
-            currentPlayer %= transform.childCount;
-            transform.GetChild(currentPlayer).gameObject.SetActive(true);
+            _playerModels[_currentPlayer].SetActive(false);
+            _currentPlayer += change + transform.childCount; //will set the index to + change 
+            _currentPlayer %= transform.childCount;
+            _playerModels[_currentPlayer].SetActive(true);
         }
 
         public void ChangeScene(string sceneName)
         {
             Transform child = prefabPlayer.transform.GetChild(0);
-            Debug.Log(currentPlayer);
+            Debug.Log(_currentPlayer);
             for (int i = 1; i < child.childCount; i++)
             {
-                child.GetChild(i).gameObject.SetActive(currentPlayer + 1 == i);
+                _playerModels[_currentPlayer].SetActive(_currentPlayer + 1 == i);
             }
             SceneManager.LoadScene(sceneName);
         }
