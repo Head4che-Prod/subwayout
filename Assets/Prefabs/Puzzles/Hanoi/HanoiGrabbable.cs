6062c27aeac44bae301a9879bf9b89c99b2c36d8
Adam-Graziani <adam.graziani@epita.fr>
2025-03-30 17:42:08 +0200

    New movement system test
    This 2D movement system decides whether to apply a strictly horizontal or vertical force

--- Assets/Prefabs/Puzzles/Hanoi/HanoiGrabbable.cs
+++ Assets/Prefabs/Puzzles/Hanoi/HanoiGrabbable.cs
@@ -30,15 +30,33 @@ namespace Prefabs.Puzzles.Hanoi
         public override Vector3 CalculateMovementForce()
         {
             if (Physics.Raycast(Owner.playerCamera.transform.position, Owner.playerCamera.transform.forward,
-                    out RaycastHit hit, 12f, 3))
+                    out RaycastHit hit, 12f, 1000))  // Only let layer 3 pass
             {
+                Vector3 direction = HanoiTowers.Instance.transform.InverseTransformVector(hit.point - transform.position);
+                direction.y = 0;
+                
+                Vector3 hitPointLocal = HanoiTowers.Instance.transform.InverseTransformDirection(hit.point);
+                Vector3 positionLocal = HanoiTowers.Instance.transform.InverseTransformDirection(transform.position);
+
+                if (direction.magnitude > 0.2f)
+                {
+                    if (Math.Abs(hitPointLocal.x - positionLocal.x) >
+                        Math.Abs(hitPointLocal.z - positionLocal.z) + 0.2f)
+                        direction.z = 0;
+                    else if (Math.Abs(hitPointLocal.z - positionLocal.z) >
+                             Math.Abs(hitPointLocal.x - positionLocal.x) + 0.2f)
+                        direction.x = 0;
+                }
+                
+                direction = HanoiTowers.Instance.transform.InverseTransformVector(direction.normalized) * 0.1f;
+                
                 if (HanoiTowers.Instance.IsInDebugMode)
                 {
                     MovementVector.Instance.SetPosition(0, transform.position);
-                    MovementVector.Instance.SetPosition(1, hit.point);
+                    MovementVector.Instance.SetPosition(1, direction);
                 }
                 
-                return hit.point - transform.position;
+                return direction;
             }
 
             Drop();
