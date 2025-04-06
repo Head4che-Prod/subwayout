27748d524d4457efd37da15e7eea8da097818623
Valentin_Kh <valentin_kh@outlook.fr>
2025-04-06 12:37:12 +0200

    Add Doors' interactions and fix PlayableCar hierarchy

---
+++ Assets/Prefabs/PlayableCar/Scripts/DoorsActionable.cs
@@ -0,0 +1,15 @@
+﻿using Objects;
+using Prefabs.GameManagers;
+using Prefabs.Player;
+
+namespace Prefabs.PlayableCar.Scripts
+{
+    public class DoorsActionable : ObjectActionable
+    {
+        protected override void Action(PlayerObject player)
+        {
+            if (TutorialManager.Singleton.State == TutorialState.TrainStopped)
+                TutorialManager.Singleton.State = TutorialState.TrainMoving;
+        }
+    }
+}
