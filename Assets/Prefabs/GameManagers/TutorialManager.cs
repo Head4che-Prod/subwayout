25de304b1d348574c702124b87160b90fe08b539
Valentin_Kh <valentin_kh@outlook.fr>
2025-04-06 12:35:41 +0200

    Create TutorialManager

---
+++ Assets/Prefabs/GameManagers/TutorialManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Objects;
+using UnityEngine;
+
+namespace Prefabs.GameManagers
+{
+    public class TutorialManager : MonoBehaviour
+    {
+        public static TutorialManager Singleton;
+        private TutorialState _state;
+        // public static event Action<TutorialState> OnStateChange;
+        
+        [Header("Train")]
+        [SerializeField] private Animator tunnelAnimator;
+        
+        [Header("EmergencyTrigger")]
+        [SerializeField] private Transform triggerGrabbableTransform;
+
+        [Header("Visuals")] 
+        [SerializeField] private Transform[] lightsTransforms;
+        
+        public TutorialState State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+
+                switch (value)
+                {
+                    case TutorialState.TrainStopped:
+                        break;
+                    case TutorialState.TrainMoving:
+                        RestartTrain();
+                        break;
+                    case TutorialState.HintSystemUnlocked:
+                        Destroy(this);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(value), value, null);
+                }
+            
+                // OnStateChange?.Invoke(value);
+            }
+        }
+
+        private void Awake() => Singleton = this;
+        private void Start() => State = TutorialState.TrainStopped;
+        
+        private void RestartTrain()
+        {
+            tunnelAnimator.SetBool(Animator.StringToHash("Move"), true);
+            HighlightTrigger();
+        }
+
+        private void HighlightTrigger() 
+            => triggerGrabbableTransform.GetComponent<ObjectOutline>().enabled = true;
+    }
+
+    public enum TutorialState
+    {
+        TrainStopped, // Waiting player to try to open doors
+        TrainMoving,
+        HintSystemUnlocked
+    }
+}
