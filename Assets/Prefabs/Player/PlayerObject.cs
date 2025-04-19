580d63546edc260e3a76cf52e17ce40b0ea30e48
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-04-20 01:39:40 +0200

    skin quick fix

--- Assets/Prefabs/Player/PlayerObject.cs
+++ Assets/Prefabs/Player/PlayerObject.cs
@@ -9,11 +9,11 @@ namespace Prefabs.Player
     public class PlayerObject : NetworkBehaviour
     {
         [NonSerialized] public int CurrentSkin = 0;
-        public PlayerInputManager InputManager {get; private set;}
-        public PlayerMovement Movement {get; private set;}
-        public PlayerInteract Interaction {get; private set;}
-        public Rigidbody Rigidbody {get; private set;}
-        public PlayerInput Input {get; private set;}
+        public PlayerInputManager InputManager { get; private set; }
+        public PlayerMovement Movement { get; private set; }
+        public PlayerInteract Interaction { get; private set; }
+        public Rigidbody Rigidbody { get; private set; }
+        public PlayerInput Input { get; private set; }
         public Camera playerCamera;
         public Transform grabPointTransform;
         public GameObject playerCharacter;
@@ -29,21 +29,18 @@ namespace Prefabs.Player
 
         public void Start()
         {
-            // foreach (PlayerObject player in GameObject.FindGameObjectsWithTag)
-            // {
-                
-            // }
-            Transform models = transform.Find("Character");
-            for (int i = 1; i < models.childCount; i++)
+            if (!IsLocalPlayer)
             {
-                models.GetChild(i).gameObject.SetActive(PlayerSelection.CurrentPlayer + 1 == i);
-            }
-
-            if (!IsLocalPlayer) {
                 transform.Find("UI").gameObject.SetActive(false);
                 transform.Find("Canvas").gameObject.SetActive(false);
                 transform.Find("CameraPos").gameObject.SetActive(false);
                 transform.Find("CameraHolder").gameObject.SetActive(false);
+            } else {
+                Transform models = transform.Find("Character");
+                for (int i = 1; i < models.childCount; i++)
+                {
+                    models.GetChild(i).gameObject.SetActive(PlayerSelection.CurrentPlayer + 1 == i);
+                }
             }
         }
     }
