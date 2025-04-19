9a54b85285f9a377f587d2c034c95162602cf917
Cospiz67 <jeannedurand2707@gmail.com>
2025-04-19 15:06:38 +0200

    anim Works

--- Assets/Prefabs/Puzzles/FoldingSeats/SingleChair.cs
+++ Assets/Prefabs/Puzzles/FoldingSeats/SingleChair.cs
@@ -2,24 +2,20 @@ using System;
 using Objects;
 using UnityEngine;
 using Prefabs.Player;
+using UnityEngine.Serialization;
 
 namespace Prefabs.Puzzles.FoldingSeats
 {
     public class SingleChair : ObjectActionable
     {
-        private Animator _chairAnimator;
         private static readonly int ChairUp = Animator.StringToHash("activateUp");
         private static readonly int ChairDown = Animator.StringToHash("ChairDown");
-
-        public void Start()
-        {
-            _chairAnimator = GetComponent<Animator>();
-        }
+        [SerializeField] private Animator chairAnimator;
         
         protected override void Action(PlayerObject player)
         {
             Debug.Log("has touched the seat");
-            _chairAnimator.SetBool(ChairUp, !_chairAnimator.GetBool(ChairUp));
+            chairAnimator.SetBool(ChairUp, !chairAnimator.GetBool(ChairUp));
         }
     }
     
