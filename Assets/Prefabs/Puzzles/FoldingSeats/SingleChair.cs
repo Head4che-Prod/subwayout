61d3741e690e957ae1b0864b7f05917f131f1b74
Cospiz67 <jeannedurand2707@gmail.com>
2025-04-20 20:15:51 +0200

    try

--- Assets/Prefabs/Puzzles/FoldingSeats/SingleChair.cs
+++ Assets/Prefabs/Puzzles/FoldingSeats/SingleChair.cs
@@ -2,6 +2,7 @@ using System;
 using Objects;
 using UnityEngine;
 using Prefabs.Player;
+using Unity.Netcode;
 
 namespace Prefabs.Puzzles.FoldingSeats
 {
@@ -10,13 +11,20 @@ namespace Prefabs.Puzzles.FoldingSeats
         private static readonly int ChairUp = Animator.StringToHash("activateUp");
         private static readonly int ChairDown = Animator.StringToHash("ChairDown");
         [SerializeField] private Animator chairAnimator;
-        private bool isUp = false;
+        private NetworkVariable<bool> isUp = new NetworkVariable<bool>(false);
+
+        private void Start()
+        {
+            isUp.Value = false;
+        }
+
         protected override void Action(PlayerObject player)
         {
             Debug.Log("has touched the seat");
             chairAnimator.SetBool(ChairUp, !chairAnimator.GetBool(ChairUp));
-            isUp = chairAnimator.GetBool(ChairUp);
+            isUp.Value = chairAnimator.GetBool(ChairUp);
         }
+
     }
     
 
