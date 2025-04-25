1e657b44ac4884b17cec684a177e9f7f317d138e
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-04-25 18:00:56 +0200

    test code interactible

--- Assets/Prefabs/Puzzles/DoorCode/Tile.cs
+++ Assets/Prefabs/Puzzles/DoorCode/Tile.cs
@@ -1,16 +1,27 @@
+using Prefabs.Player;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Tile : MonoBehaviour
+public class Tile : Objects.ObjectActionable
 {
+    protected override void Action(PlayerObject player)
+    {
+        value.Value = (byte)((value.Value + 1) % 10);
+    }
+
+
+    private Text TextField;
+    public NetworkVariable<byte> value;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        value = new NetworkVariable<byte>(0);
+        TextField = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+        TextField.text = $"{value.Value}";
+        value.OnValueChanged += (prev, newVal) =>
+        {
+            TextField.text = $"{newVal}";
+        };
     }
 }
