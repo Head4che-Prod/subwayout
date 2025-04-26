bcc944919d6f432ad91079d0e3954427b41f5bf1
Adam-Graziani <adam.graziani@epita.fr>
2025-04-26 22:54:25 +0200

    Camera is now synced to Network

--- Assets/Prefabs/Player/PlayerCam.cs
+++ Assets/Prefabs/Player/PlayerCam.cs
@@ -6,6 +6,9 @@ namespace Prefabs.Player
 {
     public class PlayerCam : NetworkBehaviour
     {
+        [Header("Camera")]
+        public GameObject playerCamera;
+        
         [Header("Sensibility")]
         public float sensX;
         public float sensY;
@@ -15,31 +18,42 @@ namespace Prefabs.Player
     
         private InputAction _lookAction;
         private PlayerObject _player;
-        void Start()
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            _player = GetComponentInParent<PlayerObject>();
-            _lookAction = _player.Input.actions["Look"];
-
-            _player.playerCamera.cullingMask &= ~(1 << 7);
         
-            _player.playerCharacter.layer = 7;
-            SetLayerAllChildren(_player.playerCharacter.transform, 7);
+        private bool _isActive;
+
+        public override void OnNetworkSpawn()
+        {
+            _isActive = IsLocalPlayer;
+            playerCamera.gameObject.SetActive(_isActive);
+            if (_isActive)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                _player = GetComponentInParent<PlayerObject>();
+                _lookAction = _player.Input.actions["Look"];
+
+                _player.playerCamera.cullingMask &= ~(1 << 7);
+
+                _player.playerCharacter.layer = 7;
+                SetLayerAllChildren(_player.playerCharacter.transform, 7);
+            }
         }
     
         void Update()
         {
-            Vector2 lookVector2 = _lookAction.ReadValue<Vector2>();
-            float lookX = lookVector2.x * Time.deltaTime * sensX;
-            float lookY = lookVector2.y * Time.deltaTime * sensY;
-		
-            _yRotation += lookX;
-            _xRotation -= lookY;
-            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+            if (_isActive)
+            {
+                Vector2 lookVector2 = _lookAction.ReadValue<Vector2>();
+                float lookX = lookVector2.x * Time.deltaTime * sensX;
+                float lookY = lookVector2.y * Time.deltaTime * sensY;
 
-            _player.transform.rotation = Quaternion.Euler(0, _yRotation, 0);
-            transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+                _yRotation += lookX;
+                _xRotation -= lookY;
+                _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+                _player.transform.rotation = Quaternion.Euler(0, _yRotation, 0);
+                transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+            }
         }
     
         private void SetLayerAllChildren(Transform root, int layer)
