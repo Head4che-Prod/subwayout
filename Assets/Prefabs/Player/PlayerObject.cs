9efd0bcf81466c0095e8c72dcd16d7086c6fc56d
Adam-Graziani <adam.graziani@epita.fr>
2025-04-29 15:17:02 +0200

    Fixed error from old code

--- Assets/Prefabs/Player/PlayerObject.cs
+++ Assets/Prefabs/Player/PlayerObject.cs
@@ -30,8 +30,7 @@ namespace Prefabs.Player
             {
                 transform.Find("UI").gameObject.SetActive(false);
                 transform.Find("Canvas").gameObject.SetActive(false);
-                transform.Find("CameraPos").gameObject.SetActive(false);
-                transform.Find("CameraHolder").gameObject.SetActive(false);
+                transform.Find("PlayerCamera").gameObject.SetActive(false);
             }
 
         }
