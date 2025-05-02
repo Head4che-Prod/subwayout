69400876db37625f8724c65c882ec9a82aa2cb77
Cospiz67 <jeannedurand2707@gmail.com>
2025-05-02 13:33:00 +0200

    rat flees and follows when necessary + animation

--- Assets/Prefabs/Puzzles/AI/AiManager.cs
+++ Assets/Prefabs/Puzzles/AI/AiManager.cs
@@ -7,13 +7,19 @@ using Prefabs.Player;
 
 public class AiManager : MonoBehaviour
 {
+    private static readonly int whichAnim = Animator.StringToHash("whichAnim");
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] GameObject cheese;
     private ObjectGrabbable _cheeseGrabbable;
+    private Animator _animator;
+    
 
     void Start()
     {
         _cheeseGrabbable = cheese.GetComponent<ObjectGrabbable>();
+        _animator = GetComponent<Animator>();
+        _animator.SetInteger(whichAnim, 0);
+        
     }
 
     private void Update()
@@ -26,9 +32,14 @@ public class AiManager : MonoBehaviour
         {
             if (Vector3.Distance(_agent.transform.position, PlayerInteract.LocalPlayerInteract.transform.position) < 5f)
             {
+                _animator.SetInteger(whichAnim, 1);
                 Vector3 direction = (PlayerInteract.LocalPlayerInteract.transform.position - transform.position).normalized;
                 _agent.speed = 5f;
                 _agent.SetDestination(transform.position - (7f*direction)); //moves in the opposite direction
+            }
+            else
+            {
+                _animator.SetInteger(whichAnim, 0);
             }
         }
     }
@@ -37,10 +48,12 @@ public class AiManager : MonoBehaviour
     {
         if (Vector3.Distance(_agent.transform.position, cheese.transform.position) < 2f)
         {
+            _animator.SetInteger(whichAnim, 0);
             _agent.destination = transform.position;
         }
         else
         {
+            _animator.SetInteger(whichAnim, -1);
             _agent.speed = 3f;
             _agent.SetDestination(cheese.transform.position);
             transform.LookAt(new Vector3(cheese.transform.position.x, cheese.transform.position.y, cheese.transform.position.z)); //face the target
