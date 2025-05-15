eeca505f7e5b6fe144b029ca24dc163ed5d6113a
Cospiz67 <jeannedurand2707@gmail.com>
2025-05-15 18:46:52 +0200

    Anim fix

--- Assets/Prefabs/Puzzles/AI/CheeseAd/TomeTomeAdManager.cs
+++ Assets/Prefabs/Puzzles/AI/CheeseAd/TomeTomeAdManager.cs
@@ -12,13 +12,24 @@ namespace Prefabs.Puzzles.AI.CheeseAd
     {
         [CanBeNull] private ObjectGrabbable _keyGrabbable;
         [SerializeField] private GameObject _keyInAd;
-
+        private Animator animator;
+        private static readonly int openCheeseAdAnim = Animator.StringToHash("openCheeseAd");
+        private readonly NetworkVariable<bool> _isOpen = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone);
+        
 
         void Start()
         {
             _keyInAd.SetActive(false);
+            animator = transform.GetChild(0).GetComponent<Animator>();
         }
-
+        
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            _isOpen.OnValueChanged += UpdatePosition;
+        }
+        
+        
         void Update()
         {
             _keyGrabbable = (ObjectGrabbable)PlayerInteract.LocalPlayerInteract.GrabbedObject;
@@ -30,10 +41,30 @@ namespace Prefabs.Puzzles.AI.CheeseAd
             {
                 DeactivateGrabbedKey();
             }
+
+            if (_keyInAd.activeInHierarchy)
+            {
+                ChangeAdDoorServerRpc(!animator.GetBool(openCheeseAdAnim));
+                Debug.Log("Cheese Ad anim is played");
+            }
         }
-    
+        
+        
+        [Rpc(SendTo.Server, RequireOwnership = false)]
+        private void ChangeAdDoorServerRpc(bool isOpenValChanged)
+        {
+            _isOpen.Value = isOpenValChanged;
+        }
         /// <summary>
-        /// Deactivate the grabbed key but activate the 3D model on in the ad.
+        /// Changes the position of the ad's door.
+        /// </summary>
+        private void UpdatePosition(bool _, bool newValue)
+        {
+            animator.SetBool(openCheeseAdAnim, newValue);
+        }
+        
+        /// <summary>
+        /// Deactivate the grabbed key but activate the 3D model in the ad.
         /// </summary>
         public void DeactivateGrabbedKey()
         {
