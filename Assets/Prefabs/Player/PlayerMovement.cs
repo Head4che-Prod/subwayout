ea6f7122cde53e4bcd8d738e02eabcce2276a0ea
Cospiz67 <jeannedurand2707@gmail.com>
2025-05-20 11:48:30 +0200

    anim Jeanne

--- Assets/Prefabs/Player/PlayerMovement.cs
+++ Assets/Prefabs/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -29,6 +30,7 @@ namespace Prefabs.Player
 		private float _verticalInput;
 
 		private PlayerObject _player;
+		private Animator _playerAnimator;
 		
 		void Start()
 		{
@@ -38,6 +40,10 @@ namespace Prefabs.Player
 			_sprintInput = _player.Input.actions["Sprint"];
 
 			_colliderHeight = playerCollider.bounds.size.y;
+			
+			int i = 1;
+			while (!transform.GetChild(0).GetChild(i).gameObject.activeInHierarchy) i++;
+			_playerAnimator = transform.GetChild(0).GetChild(i).GetComponent<Animator>();
 		}
     
 		private void LateUpdate()
@@ -61,6 +67,13 @@ namespace Prefabs.Player
 			Vector2 moveDirection = _movementInput.ReadValue<Vector2>();
 			_horizontalInput = moveDirection.x;
 			_verticalInput = moveDirection.y;
+			SendAnimRpc( _horizontalInput != 0 || _verticalInput != 0);
+		}
+
+		[Rpc(SendTo.Everyone)]
+		private void SendAnimRpc(bool setAnimation)
+		{
+			_playerAnimator.SetBool("isWalking", setAnimation);
 		}
 
 		private void MovePlayer()
