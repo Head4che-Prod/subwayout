2322c9a0f14be3ef8f16a3b373149c1a8497f57e
Kilso <95025809+Kilso80@users.noreply.github.com>
2025-05-20 22:21:06 +0200

    background noise overlap & chairs

--- Assets/Scripts/PlayBackgroundNoise.cs
+++ Assets/Scripts/PlayBackgroundNoise.cs
@@ -17,9 +17,9 @@ public class PlayBackgroundNoise : MonoBehaviour
     { 
         while(true)
         {
-            yield return new WaitForSeconds(11);
             if ((TutorialManager.Instance.State != TutorialState.TrainStopped) && EndGameManager.Instance.State == EndGameState.WaitingHanoi)
                 SoundManager.Singleton.PlaySoundRpc("BackgroundNoise", transform.position);
+            yield return new WaitForSeconds(10);
         }
     }
     private void OnDestroy()
